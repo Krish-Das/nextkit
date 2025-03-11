de556a6c469f5abc01f34efe4ad2ca2f9f6f0e08
Krish-Das <krishdasatgames@gmail.com>
2025-03-11 20:54:26 +0530

    chore: update default font to Mona-Sans and Inter
    Replaced Geist-Sans with Mona-Sans and Inter as the default fonts.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,23 @@
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
+import { Geist_Mono, Inter } from "next/font/google"
 
 import "./globals.css"
 
+import localFont from "next/font/local"
+
 import Provider from "@/components/provider"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+// https://github.com/github/mona-sans
+const monaSans = localFont({
+  src: "../../public/fonts/mona/Mona-Sans.woff2",
+  variable: "--font-mona",
+  display: "swap",
 })
-
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+})
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
@@ -28,7 +36,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body
+        className={`${monaSans.variable} ${inter.variable} ${geistMono.variable}`}
+      >
         <Provider>{children}</Provider>
       </body>
     </html>
